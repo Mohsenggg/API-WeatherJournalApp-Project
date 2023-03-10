b738fb1cfec9981d9bedcbaeac91560fb755a482
Mohsen <mohse.gamal@gmail.com>
2023-03-10 22:14:37 +0200

    Add API key and base URL

--- website/app.js
+++ website/app.js
@@ -1,5 +1,10 @@
 /* Global Variables */
 
+// API key and base URL 
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=4566baf5619ef13feaad45cb723b64d8&units=imperial';
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();

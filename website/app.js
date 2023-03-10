4d5609f5126dc5e74f38c066b6b9eac5fadea0ff
Mohsen <mohse.gamal@gmail.com>
2023-03-10 22:18:13 +0200

    Add EventListener with a Callback Function

--- website/app.js
+++ website/app.js
@@ -8,3 +8,20 @@ const apiKey = '&appid=4566baf5619ef13feaad45cb723b64d8&units=imperial';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Event listener to start when generate btn clicked 
+const btn = document.querySelector('#generate');
+btn.addEventListener('click', startSearch); 
+
+
+// Callback function for the event listener   
+function startSearch() {
+  // Get user values
+  const zipCode = document.querySelector('#zip').value; 
+  const feeling = document.querySelector('#feelings').value; 
+  // Get data from OpenWeatherMap API
+  console.log("Get data from OpenWeatherMap API");
+  console.log(`${baseURL}${zipCode}${apiKey}`);
+  addWeather(baseURL, zipCode, apiKey)
+}
